c96d98ba0f832447a2b8578240a97055c217d731
abz <s.martinez92@live.com>
2018-10-11 16:33:05 -0400

    hi

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 var app = express();
-var PORT = process.env.PORT || 4000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static(process.cwd() + '/public'));
 
