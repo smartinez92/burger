acf62e84c2d9644942af3959c0c64cd2ce9c4ac2
abz <s.martinez92@live.com>
2018-10-11 16:26:39 -0400

    hello

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name) VALUES ('Fish Fillet');
+INSERT INTO burgers (burger_name) VALUES ('Spicy Chicken Sandwich');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
